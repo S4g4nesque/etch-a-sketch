823ae6460ee7a424482bedca28c2cb67f189fa32
Pomodoro <169356952+S4g4nesque@users.noreply.github.com>
2025-07-15 15:10:49 +0200

    Create reset button using methods Need to do: position button at top of page

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector("#container");
 
 
+
 for (i = 0; i < 256; i++){
     const box = document.createElement("div");
     box.setAttribute("class", "box");
     container.appendChild(box);
 }
+
+const resetButton = document.createElement("button");
+resetButton.setAttribute("id", "resetButton");
+resetButton.textContent = "Reset Grid";
+container.appendChild(resetButton);
+
+
