daa6e23b7e69f2e02cdc7aeb5f4f38a0c80d2892
Pomodoro <169356952+S4g4nesque@users.noreply.github.com>
2025-07-16 15:52:36 +0200

    Add "mousover" event to boxes Changes color and remains that way Important note: eventListener must be within the loop because "box" is only defined in there

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ for (i = 0; i < 256; i++){
     const box = document.createElement("div");
     box.setAttribute("class", "box");
     container.appendChild(box);
+    box.addEventListener("mouseover", ()=> {box.style.backgroundColor = "blueviolet";})
+
 }
 
 const resetButton = document.createElement("button");
 resetButton.setAttribute("id", "resetButton");
 resetButton.textContent = "Reset Grid";
 document.body.insertBefore(resetButton, document.body.firstChild);
-
 
 
